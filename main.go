50096b02407898e510114402fd1bc493716e3afc
brian.ecv <56954361+brianlee19@users.noreply.github.com>
2019-11-07 02:55:07 +0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Hello Ecloudvalley"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
