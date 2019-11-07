341dacaa1d6e958af3108c787b75d453d673774f
brian.ecv <56954361+brianlee19@users.noreply.github.com>
2019-11-07 12:06:29 +0800

    Update main.go

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello Ecloudvalley"}
+		res := &response{Message: "Hello Ecloudvalley, an AWS Premier Tier Partner"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
